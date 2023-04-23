c21cdd636ed9330dc426b42026cf6c07e01b206b
Eric Savage <eric.n.savage2000@gmail.com>
2023-04-23 07:58:40 -0400

    fixed register php

--- register.php
+++ register.php
@@ -7,6 +7,37 @@
     $conID, $db;
 
     function openDatabase(&$conID){
-        $host = ""
+        $host = "fdb1027.freehostspace.com";
+        $db = "4180656_users";
+
+        $usr="4180656_users";
+        $pw = "RED-panda12";
+
+        $conID = new mysqli($host, $usr, $pw, $db);
+
+		if ($conID->connect_error) {
+            die("Connection failed: " . $conID->connect_error);
+        }
     }
+
+    function write($conID, $username, $password){
+        $SQL = " INSERT INTO 'registration' ('userName', 'password')";
+        $SQL = $SQL . " VALUES ('$username', '$password')" 
+        $result = $conID->query($SQL);
+        if(!$result){
+            die( "Error: " .$SQL. " :" .$conID->connect_error);
+        } else {
+            return true;
+        }
+    }
+
+    openDatabase($conID);
+    if(write($conID, $username, $password)){
+        header("Location: http://pixelperfectanimals.freehostspace.com/index.html"); 
+        die();
+    } else {
+        echo"<h1> User name and or password were incorrect, please try again!</h1>";
+    }
+
+    $conID->close();
 ?>
