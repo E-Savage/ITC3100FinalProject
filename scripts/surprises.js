b14fc152363ce104ec7fd7f1fc3527538c7a43a7
Eric Savage <eric.n.savage2000@gmail.com>
2023-04-23 09:21:11 -0400

    added surprise

---
+++ scripts/surprises.js
@@ -0,0 +1,25 @@
+
+function alligator(){
+    alert("The alligator became the official state reptile of Florida in 1987.");
+}
+
+function moleRat(){
+    alert("Mole rats live in burroughs.");
+}
+
+function mongoose(){
+    alert("Mongooses are related to weasels");
+}
+
+function octopus(){
+    alert("Octopuses have 3 hearts!");   
+}
+
+function redPanda(){
+    alert("Red pandas are related to raccoons and not bears.");
+}
+
+function sloth(){
+    alert("Thousands of years ago the americas had large populations of giant sloths.");
+}
+
